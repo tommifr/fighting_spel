8540b66e58b0011065ef25113a580724f045855b
Tommi Frykman <Tommi.Frykman@elev.ga.ntig.se>
2023-10-26 14:06:37 +0200

    ,jy,kjymkjy

--- fighting/Program.cs
+++ fighting/Program.cs
@@ -12,6 +12,7 @@ fighter2.weapon.name = "baseballbat";
 fighter2.weapon.dmg= 17;
 
 
+
 while(fighter1.hp > 0 && fighter2.hp > 0 ){
     Console.WriteLine($"Fighter one's hp is{fighter1.hp}, fighter two's hp is {fighter2.hp}");
     fighter1.attack(fighter2);
